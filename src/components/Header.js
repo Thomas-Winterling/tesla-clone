0514d7c2ef32289d732a335b35380150a2785b8a
Thomas Winterling <73542264+Thomas-Winterling@users.noreply.github.com>
2023-04-10 21:22:10 +0200

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,119 @@
+import React, { useState }  from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+
+function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(true);
+
+    return (
+        <Container>
+            <a>
+                <img src="/images/logo.svg" alt="" />
+            </a>
+            <Menu>
+                <p><a href="#">Model S</a></p>
+                <p><a href="#">Model 3</a></p>
+                <p><a href="#">Model X</a></p>
+                <p><a href="#">Model Y</a></p>
+            </Menu>
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+                <CustomMenu onClick={() => setBurgerStatus(false)} />
+            </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() => setBurgerStatus(true)} />
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Use Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+            </BurgerNav>
+            
+        </Container>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+ min-height: 60px;
+ position: fixed;
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+ padding: 0 20px;
+ top: 0;
+ left: 0;
+ right: 0;
+ z-index: 1;
+`
+const Menu = styled.div`
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ flex: 1;
+
+ a {
+     font-weight: 600;
+     text-transform: uppercase;
+     padding: 0 10px;
+     flex-wrap: nowrap;
+ }
+
+ @media(max-width: 768px) {
+    display: none;
+}
+ ` 
+
+ const RightMenu = styled.div`
+ display: flex;
+ align-items: center;
+ a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+    flex-wrap: nowrap;
+}
+ ` 
+
+const CustomMenu = styled(MenuIcon)`
+ cursor: pointer;
+`  
+
+const BurgerNav = styled.div`
+ position: fixed;
+ top: 0;
+ bottom: 0;
+ right: 0;
+ background-color: white;
+ width: 300px;
+ z-index: 16;
+ list-style: none;
+ padding: 20px;
+ display: flex;
+ flex-direction: column;
+ justify-content: flex-start;
+ text-align: start;
+ transform: ${props => props.show ? 'translateX(100%)' : 'translateX(0)'};
+ transition: transform 0.2s ease-in;
+ li {
+     padding: 15px 0;
+     border-bottom: 1px solid rgba(0, 0, 0, .2);
+
+     a {
+         font-weight: 600;
+     }
+ }
+`  
+const CustomClose = styled(CloseIcon)`
+ curosr: pointer;
+`  
+
+const CloseWrapper = styled.div`
+ display: flex;
+ justify-content: flex-end;
+`  
